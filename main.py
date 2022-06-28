88f88d8d55c4d0f45c8acbc728674e95184d16d2
junaid-home <jj123dev@gmail.com>
2022-06-28 17:42:39 +0500

    feat: finsh mlr endpoint

--- main.py
+++ main.py
@@ -2,19 +2,41 @@ from pickle import load
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
 slr = load(open("trained_slr_model.pkl", "rb"))
+mlr = load(open("trained_mlr_model.pkl", "rb"))
 
 
 @app.route("/slr/predict", methods=["POST"])
 def slr_pridict():
     data = request.json
+
     rooms = int(data["rooms"])
+
     predicted_price = slr.predict([[rooms]])
 
     return jsonify({
         "Status_Code": 200,
         "Model_Name": "Simple Linear Regression",
         "Predicted_Price": predicted_price[0][0],
+    }), 200
+
+
+@app.route("/mlr/predict", methods=["POST"])
+def mlr_predict():
+    data = request.json
+    bedrooms = int(data["bedrooms"])
+    bathrooms = int(data["bathrooms"])
+    floors = int(data["floors"])
+    condition = int(data["condition"])
+
+    predicted_price = mlr.predict(
+        [[bedrooms, bathrooms, floors, condition]])
+
+    return jsonify({
+        "Status_Code": 200,
+        "Model_Name": "Multiple Linear Regression",
+        "Predicted_Price": predicted_price[0],
     }), 200
 
 
